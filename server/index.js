f63d9ebb6917953258dd7d92e66f9082b98a23f7
NanaProject <nanaproject2023@yahoo.com>
2026-04-19 21:02:11 -0400

    d

--- server/index.js
+++ server/index.js
@@ -11,9 +11,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: "https://nana-project-portfolio-o795.vercel.app"
-}));
+app.use(cors());
 app.use(express.json());
 
 const pool = new Pool(
@@ -152,6 +150,8 @@ app.delete("/transactions/:id", authenticateToken, async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
