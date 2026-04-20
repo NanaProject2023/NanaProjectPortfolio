5ef6a12bbccd33a06e2bbdb925e9a0f7448a3b1b
NanaProject <nanaproject2023@yahoo.com>
2026-04-19 20:47:13 -0400

    na

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -10,7 +10,7 @@ function Signup() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/signup", {
+      const res = await axios.post("https://nanaprojectportfolio.onrender.com/signup", {
         email,
         password,
       });
